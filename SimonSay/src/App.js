4698a77d2d53f15fb3e669c66193d42f8505c9c8
AVAVT <itsavavt@gmail.com>
2018-01-08 22:14:13 +0700

    Homework template

--- SimonSay/src/App.js
+++ SimonSay/src/App.js
@@ -6,67 +6,23 @@ import {
   Button
 } from 'react-native';
 
-import ColorButton from "./components/ColorButton";
+import GamePlay from './containers/GamePlay';
+import GameOver from './containers/GameOver';
 
-// const Text2 = (props) => <Text>{this.props.children}</Text>
+const GAME_STATE = {
+  PLAYING : "PLAYING",
+  GAMEOVER: "GAMEOVER"
+}
 
 export default class App extends Component {
   state = {
     score: 0,
-    targetInput: [],
-    userInputIndex: 0
-  }
-
-  _onPress = (input) => {
-    const {targetInput, userInputIndex, score} = this.state;
-    
-    input === targetInput[userInputIndex]
-      && this.setState({
-        score: score + 1,
-        userInputIndex: userInputIndex + 1
-      });
-  }
-
-  _randomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  componentDidMount() {
-    this.setState({
-      targetInput: Array.from({ length: 6 }, item => this._randomInt(0, 4))
-    })
+    gameState: GAME_STATE.PLAYING
   }
 
   render() {
-    return (
-      <View>
-        <Text>Hello React Native!</Text>
-        <Text>{this.state.score}</Text>
-        <Text>{this.state.targetInput}</Text>
-        <ColorButton onPress={() => this._onPress(0)} background="red" />
-        <ColorButton onPress={() => this._onPress(1)} background="yellow" />
-        <ColorButton onPress={() => this._onPress(2)} background="blue" />
-        <ColorButton onPress={() => this._onPress(3)} background="green" />
-      </View>
-    );
+    return this.state.gameState === GAME_STATE.PLAYING
+      ? <GamePlay/>
+      : <GameOver score={this.state.score} />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
